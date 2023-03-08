0c37bc9107959537a067f4731b50f1568720c5fb
Zef Anderson <zeferino@Zefs-MacBook-Pro.local>
2023-03-08 12:08:43 -0700

    all tests passed

--- queue.js
+++ queue.js
@@ -1,25 +1,29 @@
-const sKey = {};
-const items = new WeakMap();
+// const sKey = {};
+// const items = new WeakMap();
 
 class Stack {
     constructor() {
-        items.set(sKey, [])
+    //    items.set(sKey, [])
+    this.items = [];
     }
 
     peek() {
-
+        return this.items[this.items.length - 1];
     }
 
     pop() {
-
+        if(this.items.length == 0)
+            return undefined;
+        return this.items.pop();
+        
     }
 
-    push() {
-
+    push(data) {
+        this.items.push(data);
     }
 
     size() {
-
+        return this.items.length;
     }
 
 }

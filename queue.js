452046294b5169fe318a1e7a2c44b023443d9464
Zef Anderson <zeferino@Zefs-MacBook-Pro.local>
2023-03-08 10:53:12 -0700

    tests are ready and I've just started setting up functions

---
+++ queue.js
@@ -0,0 +1,25 @@
+const sKey = {};
+const items = new WeakMap();
+
+class Stack {
+    constructor() {
+        items.set(sKey, [])
+    }
+
+    peek() {
+
+    }
+
+    pop() {
+
+    }
+
+    push() {
+
+    }
+
+    size() {
+
+    }
+
+}
